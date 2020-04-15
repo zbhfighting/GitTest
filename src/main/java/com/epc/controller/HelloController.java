1afae049f3a0ee99b9cd30a86cdfe0602bde7eba
zbhfighting <545500416@qq.com>
2020-04-15 09:57:22 +0800

    修改controller为restcontroller

--- src/main/java/com/epc/controller/HelloController.java
+++ src/main/java/com/epc/controller/HelloController.java
@@ -3,8 +3,9 @@ package com.epc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(Model model){
