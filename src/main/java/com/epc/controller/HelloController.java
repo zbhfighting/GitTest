f1f1164596b16078aec2c9c1876b8c62f1d5ca84
zbhfighting <545500416@qq.com>
2020-04-15 09:41:47 +0800

    测试提交

---
+++ src/main/java/com/epc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.epc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","AAAAAAA");
+        return "hello spring boot \n 谢谢阅读!";
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangting
+  Date: 2020/4/7
+  Time: 16:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>测试测试测试</title>
+    <p>${msg}</p>
+</head>
+<body>
+
+</body>
+</html>
